472a5915e1553e2089f10c502d31b9840683a5e7
Emre Can Serteli <epicum@gmail.com>
2022-04-26 22:54:52 +0300

    Add project files.

---
+++ ICompositionRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace TutorialDependencyInjection
+{
+    public interface ICompositionRoot
+    {
+        void LogMessage(string message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using System;
+
+namespace TutorialDependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+            container.Register(Component.For<ICompositionRoot>().ImplementedBy<CompositionRoot>());
+
+            var root = container.Resolve<ICompositionRoot>();
+            root.LogMessage("Hello from my very first resolved class!");
+
+            Console.ReadLine();
+        }
+    }
+}
